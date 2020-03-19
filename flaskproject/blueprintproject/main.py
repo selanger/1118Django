3ea9f7b95d4044e2a3fc06ac7399108aa57ab2f6
wjp <str_wjp@126.com>
2020-03-19 16:53:16 +0800

    flask script

--- flaskproject/blueprintproject/main.py
+++ flaskproject/blueprintproject/main.py
@@ -1,12 +1,26 @@
 ### 项目的控制文件
 from blueprintproject import createApp
+from flask_script import Manager,Command
 app = createApp("settings.Config")
+## 管理app
+manager = Manager(app)
+class Hello(Command):   ## 自定义命令   需要继承 Command父类
+    def run(self):     ## 重写run方法
+        ### 命令执行内容
+        print("HELLO world")
+### python manage.py runsever8000   项目启动在 127.0.0.1:8000 端口
+class Runserver(Command):
+    def run(self):
+        app.run(port=8000)
 
+manager.add_command("hello",Hello)    ### 添加命令  第一个参数： 命令的名字  第二个参数： 命令的内容
+manager.add_command("runserver8000",Runserver)
 if __name__ == '__main__':
     # from course.models import *
     # from user.models import *
     # db.create_all()
-    app.run(debug=True)
+    # app.run(debug=True)
+    manager.run()
 
 
 
